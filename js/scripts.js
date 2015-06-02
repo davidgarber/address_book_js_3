3c4609777efd631cc7a36bb061ab8ebd3325cfd9
Leon Zagorouiko & David Garber <student@epicodus.com>
2015-06-02 10:28:38 -0700

    css

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,23 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-function Address(street, city, state) {
+function Address(street, city, state, addresstype) {
   this.street = street;
   this.city = city;
   this.state = state;
+  this.addresstype = addresstype;
 }
 
 Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state;
+  return this.street + ", " + this.city + ", " + this.state + ", " + this.addresstype;
 }
+
 
 function resetFields(){
   $("input#new-first-name").val("");
   $("input#new-last-name").val("");
   $("input.new-street").val("");
-  $("input.new-city").val("");
+  $("input.new-city").val("");;
   $("input.new-state").val("");
   $("div.new-address").not(':first').remove();
 }
@@ -42,6 +44,10 @@ $(document).ready(function() {
                                    '<label for="new-state">State</label>' +
                                    '<input type="text" class="form-control new-state">' +
                                  '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-type">Address Type</label>' +
+                                   '<input type="text" class="form-control new-type">' +
+                                 '</div>' +
                                '</div>');
 });
 
@@ -57,8 +63,9 @@ $("form#new-contact").submit(function(event) {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
+      var inputtedType = $(this).find("input.new-type").val();
 
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedType)
       newContact.addresses.push(newAddress);
     });
 
@@ -66,9 +73,14 @@ $("form#new-contact").submit(function(event) {
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
-      $("#show-contact").show();
+      $("#show-contact").fadeIn("slow", function() {
+    });
 
-      $("#show-contact h2").text(newContact.fullName());
+      $("#show-contact h2").text(newContact.fullName())
+      $(".first-name").hover(function() {
+        $(this).css("background-color",
+        "pink");
+      });
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
 
